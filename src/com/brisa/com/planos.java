0594b330cc855adb7410cb53406f8ac8e543aea9
bertulio40 <109047366+bertulio40@users.noreply.github.com>
2022-12-04 10:27:09 -0300

    brisa.com

---
+++ src/com/brisa/com/planos.java
@@ -0,0 +1,26 @@
+package com.brisa.com;
+
+import javax.swing.JOptionPane;
+
+public class planos {
+
+public static void main(String... args){
+
+        String nome;
+        int cpf;
+        String plano;
+
+
+    nome =  JOptionPane.showInputDialog("Nome Do Cliente");
+
+    String cpf_do_cliente = JOptionPane.showInputDialog("CPF Do Cliente");
+
+    plano =  JOptionPane.showInputDialog("Plano do cliente ");
+
+
+
+
+
+
+}
+}
